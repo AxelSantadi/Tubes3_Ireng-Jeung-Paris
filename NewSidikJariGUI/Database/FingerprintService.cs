9352d8ce3b950a58598547f4319b80c6da4b3d5d
ozarabal <r.ardhinto@gmail.com>
2024-06-08 19:47:40 +0700

    algoCompare

--- NewSidikJariGUI/Database/FingerprintService.cs
+++ NewSidikJariGUI/Database/FingerprintService.cs
@@ -46,4 +46,30 @@ public class FingerprintService
         return berkasCitraList;
     }
 
+    public string getBerkascitrabyAlgo(string berkasCitra, string algo)
+    {
+        var berkasCitraList = GetAllBerkasCitra();
+        var Asciiinput = FingerprintConverter.ConvertToBinary(berkasCitra);
+        string result = "";
+        for (int i = 0; i < berkasCitraList.Count; i++)
+        {
+            var berkas = berkasCitraList[i].BerkasCitra;
+            var Ascii = FingerprintConverter.ConvertToBinary(berkas);
+            double percentage = 0;
+            if (algo == "BM")
+            {
+                percentage = BoyerMooreAlgorithm.BoyerMooreSearch(Ascii, Asciiinput);
+            }
+            else if (algo == "KMP")
+            {
+                percentage = KMPAlgorithm.KMPSearch(Ascii, Asciiinput);
+            }
+            if (percentage > 85)
+            {
+                return berkas;
+            }
+        }
+        return result;
+    }
+
 }
