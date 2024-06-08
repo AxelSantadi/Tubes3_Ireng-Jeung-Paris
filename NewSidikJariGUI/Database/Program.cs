9b3208f4a7b159fdc851f922e1ce52d1a0ad4df5
ozarabal <r.ardhinto@gmail.com>
2024-06-08 18:52:20 +0700

    push faker

--- NewSidikJariGUI/Database/Program.cs
+++ NewSidikJariGUI/Database/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bogus;
 
 
 public class Program
@@ -20,62 +21,59 @@ public class Program
         db.CreateTable<SidikJari>();
 
         string fingerprintDirectory = @"assets";
-        
-        foreach (var filePath in Directory.GetFiles(fingerprintDirectory, "*.BMP"))
+
+        var fakerBiodata = new Faker<Biodata>()
+            .RuleFor(b => b.NIK, (f, b) => int.Parse(f.Random.ReplaceNumbers("#########")) * 1000)
+            .RuleFor(b => b.Nama, f => Biodata.NormalizeNama(f.Name.FullName()))
+            .RuleFor(b => b.TempatLahir, f => f.Address.City())
+            .RuleFor(b => b.TanggalLahir, f => f.Date.Past(30, DateTime.Now.AddYears(-18)))
+            .RuleFor(b => b.JenisKelamin, f => f.PickRandom<JenisKelamin>())
+            .RuleFor(b => b.GolonganDarah, f => f.PickRandom(new[] { "A", "B", "AB", "O" }))
+            .RuleFor(b => b.Alamat, f => f.Address.FullAddress())
+            .RuleFor(b => b.Agama, f => f.PickRandom(new[] { "Islam", "Kristen", "Katolik", "Hindu", "Buddha", "Konghucu" }))
+            .RuleFor(b => b.StatusPerkawinan, f => f.PickRandom<StatusPerkawinan>())
+            .RuleFor(b => b.Pekerjaan, f => f.Name.JobTitle())
+            .RuleFor(b => b.Kewarganegaraan, f => "Indonesia");
+
+        // Get all fingerprint files
+        var files = Directory.GetFiles(fingerprintDirectory, "*.BMP");
+        var groupedFiles = files.GroupBy(f => Path.GetFileNameWithoutExtension(f).Split('_')[0]);
+
+        int counter = 1;
+        foreach (var group in groupedFiles)
         {
-            string fileName = Path.GetFileNameWithoutExtension(filePath);
+            int nikSuffix = counter % 600 + 1;
+            int nik = (fakerBiodata.Generate().NIK / 1000) * 1000 + nikSuffix;
+            counter++;
 
-            // Assuming fileName follows the format "NIK_Nama.jpg"
-            var parts = fileName.Split('_');
-            if (parts.Length < 2) continue;
-
-            int nik;
-            if (!int.TryParse(parts[0], out nik)) continue;
-
-            string nama = parts[1] + nik.ToString();
-
-            // Insert or update the biodata record if it doesn't exist
+            // Check if biodata exists for this NIK
             var biodata = db.Find<Biodata>(nik);
             if (biodata == null)
             {
-                biodata = new Biodata
-                {
-                    NIK = nik,
-                    Nama = nama,
-                    TempatLahir = null,
-                    TanggalLahir = null,
-                    JenisKelamin = null,
-                    GolonganDarah = null,
-                    Alamat = null,
-                    Agama = null,
-                    StatusPerkawinan = null,
-                    Pekerjaan = null,
-                    Kewarganegaraan = null
-                };
-                try
-                {
-                    db.Insert(biodata);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Failed to insert biodata: {e}");
-                }
+                biodata = fakerBiodata.Generate();
+                biodata.NIK = nik;
+                db.Insert(biodata);
             }
 
-            // Insert the fingerprint record
-            var sidikJari = new SidikJari
+            foreach (var filePath in group)
             {
-                BerkasCitra = filePath,
-                Nama = nama,
-            };
-            db.Insert(sidikJari);
+                var sidikJari = new SidikJari
+                {
+                    BerkasCitra = filePath,
+                    Nama = biodata.Nama
+                };
+                db.Insert(sidikJari);
+            }
         }
 
-        // Membaca dan menampilkan data
+        // Query and display the data
         Console.WriteLine("Querying for biodata");
-        
+
         var results = db.Table<Biodata>().ToList();
         var sidikJariResults = db.Table<SidikJari>().ToList();
+
+        // Membaca dan menampilkan data
+        
         // foreach (var s in results)
         // {
         //     Console.WriteLine($"NIK: {s.NIK}, Nama: {s.Nama}, Alamat: {s.Alamat}, Agama: {s.Agama}, Status Perkawinan: {s.StatusPerkawinan}, Pekerjaan: {s.Pekerjaan}, Kewarganegaraan: {s.Kewarganegaraan}, Jenis Kelamin: {s.JenisKelamin}, Golongan Darah: {s.GolonganDarah}");
@@ -86,29 +84,14 @@ public class Program
         // }
         var databaseService = new FingerprintService(db);
 
-        string targetNama = "99";
-        var biodatas = databaseService.GetBiodataByNama(targetNama);
-        foreach (var biodata in biodatas)
-        {
-            Console.WriteLine(biodata.NIK);
-            Console.WriteLine(biodata.Nama);
-            Console.WriteLine(biodata.TempatLahir);
-            Console.WriteLine(biodata.TanggalLahir);
-            Console.WriteLine(biodata.JenisKelamin);
-            Console.WriteLine(biodata.GolonganDarah);
-            Console.WriteLine(biodata.Alamat);
-            Console.WriteLine(biodata.Agama);
-            Console.WriteLine(biodata.StatusPerkawinan);
-            Console.WriteLine(biodata.Pekerjaan);
-            Console.WriteLine(biodata.Kewarganegaraan);
-        }
-
         var berkasCitraList = databaseService.GetAllBerkasCitra();
-        foreach (var berkasCitra in berkasCitraList)
-        {
-            Console.WriteLine(berkasCitra.BerkasCitra);
-        }
 
+        for (int i = 0; i < berkasCitraList.Count; i++) {
+            var berkasCitra = berkasCitraList[i];
+            var berkas = berkasCitra.BerkasCitra;
+            var nama = berkasCitra.Nama;
+            Console.WriteLine($"Nama: {nama}, Berkas: {berkas}");
+        }
         db.Close();
     }
 }
